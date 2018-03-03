76784361ffc47936ce240745a4f57997c9510506
Adam Schaal <amschaal@ucdavis.edu>
2018-03-02 16:46:48 -0800

    fix ordering

--- dnaorder/api/viewsets.py
+++ dnaorder/api/viewsets.py
@@ -6,3 +6,4 @@ class SubmissionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Submission.objects.select_related('type').all()
     serializer_class = SubmissionSerializer
     filter_fields = {'id':['icontains','exact'],'phone':['icontains'],'name':['icontains'],'email':['icontains'],'pi_name':['icontains'],'pi_email':['icontains'],'institute':['icontains'],'type__name':['icontains']}
+    ordering_fields = ['id', 'phone','name','email','pi_name','pi_email','institute','type__name','submitted']
