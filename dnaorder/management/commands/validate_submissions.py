00b97e16ab2555e0aac731a645a7d5964a9e9e9e
Adam Schaal <amschaal@ucdavis.edu>
2018-05-24 10:36:36 -0700

    Exception handling for validate management command

--- dnaorder/management/commands/validate_submissions.py
+++ dnaorder/management/commands/validate_submissions.py
@@ -7,8 +7,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Validate submissions')
         for s in Submission.objects.filter(status__isnull=False).order_by('-submitted'):
-            errors = s.samplesheet.validate()
-            if len(errors) > 0:
+            try:
+                errors = s.samplesheet.validate()
+                if len(errors) > 0:
+                    print s
+                    print "**************ERRORS******************"
+                    print errors
+            except Exception, e:
                 print s
-                print "**************ERRORS******************"
-                print errors
+                print '********Validation encountered an exception*********'
+                print e
