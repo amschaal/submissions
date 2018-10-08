61bd76d785726c3ed5ee1e3ff94434a5c7d59069
Adam Schaal <amschaal@ucdavis.edu>
2018-10-08 13:28:26 -0700

    Fixed some brokenness with validators

--- dnaorder/validators.py
+++ dnaorder/validators.py
@@ -32,6 +32,38 @@ class ValidationException(Exception):
 def get_column(variable, data=[]):
     return [line.get(variable, None) for line in data]
 
+class BaseValidator:
+    id = None #Must override this to something unique
+    name = None #Must override
+    description = None
+    def __init__(self, options= {}):
+        self.options = options
+        if not self.id:
+            raise Exception('You must define "id" for each validator')
+        if not self.name:
+            raise Exception('You must define "name" for each validator')
+    def validate(self, variable, value, schema={}, data=[]):
+        raise NotImplementedError
+
+class UniqueValidator(BaseValidator):
+    id = 'unique'
+    name = 'Unique'
+    def validate(self, variable, value, schema={}, data=[]):
+        col = get_column(variable, data)
+        valid = len([val for val in col if val == value and val is not None]) < 2
+        if not valid:
+            raise ValidationException(variable, value, 'Value "{0}" is not unique for column "{1}"'.format(value, variable))
+
+class FooValidator(BaseValidator):
+    id = 'foo'
+    name = 'Foo'
+    def validate(self, variable, value, schema={}, data=[]):
+        if value != 'foo':
+            raise ValidationException(variable, value, 'Value must be "foo"'.format(value, variable))
+
+VALIDATORS = [UniqueValidator, FooValidator]
+
+
 def unique_validator(variable, value, schema={}, data=[]): #make this a class?
     col = get_column(variable, data)
     valid = len([val for val in col if val == value and val is not None]) < 2
@@ -44,10 +76,14 @@ def get_validator(id, options):
         return unique_validator
 
 class SamplesheetValidator:
+    validators = dict([(v.id, v) for v in VALIDATORS])
     def __init__(self, schema, data):
         self.errors = {}
         self.schema = schema
         self.data = data
+    def get_validator(self, id, options):
+        if self.validators.has_key(id):
+            return self.validators[id](options)
     def set_error(self, index, variable, message):
         if not self.errors.has_key(index):
             self.errors[index] = {}
@@ -80,15 +116,15 @@ class SamplesheetValidator:
     def validate(self):
         self.errors = self.validate_jsonschema()
         for variable in self.schema['properties'].keys():
-            validators = [get_validator(v.get('id'),v.get('options',{})) for v in self.schema['properties'][variable].get('validators', [])]
-            if self.schema['properties'][variable].get('unique', False):
-                validators.append(unique_validator)
+            validators = [self.get_validator(v.get('id'),v.get('options',{})) for v in self.schema['properties'][variable].get('validators', [])]
+#             if self.schema['properties'][variable].get('unique', False):
+#                 validators.append(unique_validator)
             for idx, row in enumerate(self.data):
                 value = row.get(variable, None)
                 for validator in validators:
                     if validator:
                         try:
-                            validator(variable, value, self.schema, self.data)
+                            validator.validate(variable, value, self.schema, self.data)
                         except ValidationException, e:
                             self.set_error(idx, variable, e.message)
         return self.errors
