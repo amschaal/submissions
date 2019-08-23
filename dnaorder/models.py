5ae5af2ea519e11b1372b7982e303da8002c1f97
adam <amschaal@gmail.com>
2019-08-23 15:52:07 -0700

    store standardized variables per lab

--- dnaorder/models.py
+++ dnaorder/models.py
@@ -40,7 +40,8 @@ class Lab(models.Model):
     site = models.OneToOneField(Site)
     payment_type_id = models.CharField(max_length=30, choices=PaymentTypeManager().get_choices()) # validate against list of configured payment types
     home_page = models.TextField(default='')
-    variables = JSONField(default=dict)
+    submission_variables = JSONField(default=dict)
+    sample_variables = JSONField(default=dict)
     def __unicode__(self):
         return self.name
 
