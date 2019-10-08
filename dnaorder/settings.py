a141a00e116bfa92eda040c298b52f91bc5b8d3d
adam <amschaal@gmail.com>
2019-10-08 16:14:59 -0700

    Cleaning up libraries

--- dnaorder/settings.py
+++ dnaorder/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_filters',
+#     'rest_framework_filters',
     'django_filters',
 #     'material',
     'dnaorder',
