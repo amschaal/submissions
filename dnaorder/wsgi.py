17eb34d4a3601282c73c5987477fa2d3a641f1d1
adam <amschaal@gmail.com>
2019-10-11 11:21:53 -0700

    Trying to fix wsgi for python 3 virtualenv

--- dnaorder/wsgi.py
+++ dnaorder/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
+import os, sys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'submissions.settings')
+
+sys.path.append(os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2]))
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
