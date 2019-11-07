b29fb2d09b2b7bdc91a8e0ff73c3c0692e4e5c15
adam <amschaal@gmail.com>
2019-11-07 14:22:09 -0800

    issue #57

--- genomics/validators.py
+++ genomics/validators.py
@@ -1,4 +1,5 @@
-from dnaorder.validators import BaseValidator, ValidationError, ValidationWarning
+from dnaorder.validators import BaseValidator, ValidationError, ValidationWarning,\
+    MultiValidationException
 from genomics.barcodes import get_all_conflicts
 class BarcodeValidator(BaseValidator):
     id = 'barcode'
@@ -22,11 +23,38 @@ class BarcodeValidator(BaseValidator):
             raise ValidationWarning(variable, value, error)
     def _validate_all(self, schema, data, variable):
         libraries = []
-        for d in data:
+        for idx, d in enumerate(data):
             if d.get(variable, None):
                 barcodes = {variable:d.get(variable).split(',')}
                 if self.barcode2 and d.get(self.barcode2, False):
                     barcodes[self.barcode2] = d.get(self.barcode2).split(',')
-                libraries.append({'id': d.get(self.sample_name),'pool': d.get(self.pool,''),'barcodes': barcodes })
+                libraries.append({'id': idx,'pool': d.get(self.pool,''),'barcodes': barcodes, 'row': idx}) #d.get(self.sample_name)
 #         libraries = [{'id': d.get(self.sample_name),'pool': d.get(self.pool,''),'barcodes': {variable:d.get(variable).split(','), self.barcode2:d.get(self.barcode2).split(',')} } for d in data if d.get(variable, None)]
         self.errors = get_all_conflicts(libraries, self.hamming_distance)
+    def validate_all(self, variable, schema={}, data=[]):
+        self.hamming_distance = int(self.options.get('distance', 1))
+        self.pool = self.options.get('pool', None)
+        self.sample_name = self.options.get('samplename')
+        self.barcode2 = self.options.get('barcode2', None)
+        libraries = []
+        for idx, d in enumerate(data):
+            if d.get(variable, None):
+                barcodes = {variable:d.get(variable).split(',')}
+                if self.barcode2 and d.get(self.barcode2, False):
+                    barcodes[self.barcode2] = d.get(self.barcode2).split(',')
+                libraries.append({'id': idx,'pool': d.get(self.pool,''),'barcodes': barcodes })
+#         libraries = [{'id': d.get(self.sample_name),'pool': d.get(self.pool,''),'barcodes': {variable:d.get(variable).split(','), self.barcode2:d.get(self.barcode2).split(',')} } for d in data if d.get(variable, None)]
+        
+        self.errors = get_all_conflicts(libraries, self.hamming_distance)
+        print("ERRORS!!!!!!",self.errors)
+        exceptions = {}
+        for idx, row in enumerate(data):
+            if idx in self.errors:
+                if not idx in exceptions:
+                    exceptions[idx] = []
+                warning = 'Barcode conflicts with sample rows: {}.  Please ensure equal length barcodes and a hamming distance of at least {}.'.format(', '.join([str(i+1) for i in self.errors[idx].keys()]),self.hamming_distance)
+                exceptions[idx].append(ValidationWarning(variable, None, warning))
+                if self.barcode2:
+                    exceptions[idx].append(ValidationWarning(self.barcode2, None, warning))
+        if len(exceptions) > 0:
+            raise MultiValidationException(exceptions)
