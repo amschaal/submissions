e876251aa79a26b691966105267618bf7144e40d
adam <amschaal@gmail.com>
2020-03-04 12:12:54 -0800

    use variable names for export headers instead of titles

--- dnaorder/spreadsheets/__init__.py
+++ dnaorder/spreadsheets/__init__.py
@@ -1,5 +1,5 @@
 import tablib
-def get_headers(schema, use_title=True):
+def get_headers(schema, use_title=False):
     return [schema['properties'][v].get('title') if use_title and 'title' in schema['properties'][v] else v for v in schema['order']]
 
 def get_data(schema, data, default=None, list_delimiter=', '):
@@ -12,18 +12,18 @@ def get_data(schema, data, default=None, list_delimiter=', '):
         rows.append([list_delimiter.join(row.get(v,default)) if isinstance(row.get(v,default), list) else row.get(v,default) for v in schema.get('order',[])])
     return rows
 
-def get_dataset(schema, data):
-    dataset = tablib.Dataset(headers=get_headers(schema))
+def get_dataset(schema, data, use_titles=False):
+    dataset = tablib.Dataset(headers=get_headers(schema, use_titles))
     dataset.extend(get_data(schema, data))
     return dataset
 
-def get_submission_dataset(submission):
-    dataset = tablib.Dataset(headers=get_submission_headers(submission))
+def get_submission_dataset(submission, use_titles=False):
+    dataset = tablib.Dataset(headers=get_submission_headers(submission,use_titles))
     dataset.extend(get_submission_data(submission))
     return dataset
 
-def get_submission_headers(submission):
-    return ['ID', 'Internal ID', 'Type', 'First Name', 'Last Name', 'Submitter Email', 'Submitter Phone', 'PI First Name', 'PI Last Name', 'PI Email', 'PI Phone', 'Institute'] + get_headers(submission.submission_schema)
+def get_submission_headers(submission, use_title=False):
+    return ['ID', 'Internal ID', 'Type', 'First Name', 'Last Name', 'Submitter Email', 'Submitter Phone', 'PI First Name', 'PI Last Name', 'PI Email', 'PI Phone', 'Institute'] + get_headers(submission.submission_schema, use_title)
 
 def get_submission_data(submission):
     return [[submission.id, submission.internal_id, str(submission.type), submission.first_name, submission.last_name, submission.email, submission.phone, submission.pi_first_name, submission.pi_last_name, submission.pi_email, submission.pi_phone, submission.institute] + get_data(submission.submission_schema, submission.submission_data)[0]]
