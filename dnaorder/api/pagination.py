c297245a224c8442e13506fb831b2cd4a9749499
adam <amschaal@gmail.com>
2021-10-19 13:14:33 -0700

    once more place to make larger pagination

--- dnaorder/api/pagination.py
+++ dnaorder/api/pagination.py
@@ -2,4 +2,4 @@ from rest_framework.pagination import PageNumberPagination
 class StandardPagePagination(PageNumberPagination):
     page_size = 1000
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = 10000
