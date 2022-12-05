f6f1cc9012b3f6771e46bbe052db75d3eff8adfb
Adam Schaal <amschaal@gmail.com>
2022-12-05 15:38:27 -0800

    Debugging the plugin manager.  Added convenience decorator for submission based plugin views.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.utils.module_loading import import_string
 from django.conf.urls import url
 from django.urls.conf import include
+from functools import wraps
 plugin_urls = []
 
 class Plugin(object):
@@ -19,16 +20,20 @@ class Plugin(object):
 class PluginManager():
     __instance = None
     def __new__(cls, val=None):
-        if PluginManager.__instance is None:
+        if PluginManager.__instance is None or True:
             PluginManager.__instance = object.__new__(cls)
-            PluginManager.__instance.url_patterns = []
+            PluginManager.__instance.url_patterns = []#[url(r'^api/plugins/{}/submissions/(?P<submission_id>[0-9a-f-]+)/'.format('ppms'), include('plugins.{}.urls'.format('ppms')))]
             PluginManager.__instance.plugins = {}
             PluginManager.__instance.val = val
     #         PluginManager.__instance.configure_urls()
             for plugin in settings.PLUGINS:
                 _plugin = Plugin(plugin)
                 PluginManager.__instance.plugins[plugin] = _plugin
-                PluginManager.__instance.url_patterns.append(url(r'^api/{}/'.format(plugin), include(_plugin.url_patterns)))
+                PluginManager.__instance.url_patterns.append(url(r'^api/plugins/{}/submissions/(?P<submission_id>[0-9a-f-]+)/'.format(plugin), include(_plugin.url_patterns)))
+                # try:
+                #     PluginManager.__instance.url_patterns.append(url(r'^plugins/{}/'.format(plugin), include('plugins.{}.urls'.format(plugin))))
+                # except:
+                #     pass
         return PluginManager.__instance
 #     def configure_urls(self):
 #         self.__instance.url_patterns = []
@@ -44,3 +49,13 @@ class PluginManager():
         return self.__instance.url_patterns
     def get_plugin(self, plugin_id):
         return self.__instance.plugins.get(plugin_id)
+
+
+# This decorator will take the submission_id from the url, get the submission, and pass it into the original view
+def plugin_submission_decorator(view_func, foo='bar'):
+    from dnaorder.models import Submission
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        kwargs['submission']  = Submission.objects.get(id=kwargs.pop('submission_id'))
+        return view_func(request, *args, **kwargs)
+    return wrapper
